79cb44249e770c741de466b8b36defdf808e33cf
mediapam <117753295+mediapam@users.noreply.github.com>
2022-11-08 10:23:37 -0500

    Add files via upload

---
+++ client.js
@@ -0,0 +1,57 @@
+
+var iterationcard = 1;
+while(iterationcard <= channelcount) {
+
+if (iterationcard == 1) {
+	var htmlcard = '<div class="channel_'+iterationcard+' card activee"><img src="'+imgUrl+'" class="channelImage"><div class="card-content"><div class="channelName">'+chanName+'</div><div class="odometer subscriberCount">0</div></div></div>';
+} else {
+	var htmlcard = '<div class="channel_'+iterationcard+' card"><img src="'+imgUrl+'" class="channelImage"><div class="card-content"><div class="channelName">'+chanName+'</div><div class="odometer subscriberCount">0</div></div></div>';
+}
+$('body').append(htmlcard);
+iterationcard++;
+};
+
+		function fn60sec() {
+
+			
+		var googleApiCallURL="";
+		var channelList = [];
+		var channelName = [];
+		var channelImage = [];
+		var subscriberCount = [];
+
+		
+		$.getJSON('https://compteur4100.000webhostapp.com/log/channels.json', function(channels){
+        
+		channelList = channels.reverse();
+		
+console.log("Array >",Math.floor(Math.random()*Math.floor(APIKeys.length)));
+				googleApiCallURL="https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&id=" + channelList + "&key="+ APIKeys[Math.floor(Math.random()*Math.floor(APIKeys.length))];
+				
+				$.getJSON(googleApiCallURL, function(result){
+					console.log(result);
+					ytJSON = result;
+		  			var iteration = 0;
+		  	   		while (iteration <= Math.min(channelcount-1,result.items.length-1)) {
+   	   					var channelNumber = channelList.indexOf(result.items[iteration].id) + 1;
+						channelName[iteration] = result.items[iteration].snippet.title;
+						subscriberCount[iteration] = result.items[iteration].statistics.subscriberCount;
+						channelImage[iteration] = result.items[iteration].snippet.thumbnails.medium.url;
+
+						$(".channel_" + channelNumber + " .channelName").html('🎄🎁'+channelName[iteration]);
+						$('.channel_' + channelNumber + ' .subscriberCount').html(subscriberCount[iteration]);
+						$('.channel_' + channelNumber + ' .channelImage').attr('src',channelImage[iteration]);
+
+						iteration++;
+					};
+					
+				});
+			});
+			}
+
+		
+
+		
+		fn60sec();
+		setInterval(fn60sec, 45000);
+
---
+++ config.js
@@ -0,0 +1,20 @@
+/*
+Each API call costs 5 units!
+Single key can refresh the wall every 45 seconds, while 9 keys are needed for a refresh rate 
+of 5 seconds!
+*/
+
+var APIKeys = ["AIzaSyD4xUfPIN5Qs7X12dWw2gGtW5Bfjy1LQwE"];
+
+var channelcount = 20;
+
+//Default Name
+var chanName = "Mediatech";
+
+//Background Color
+var bgColor = "rgb(255, 255, 255) transparent";
+
+//Default Image url 
+var imgUrl = "https://lh3.googleusercontent.com/a-/AOh14Gidkmq2RQ8pKFBNm59bdFQ-nqV6ugR7YEc0nJx3Wg=s88-c-k-c0x00ffffff-no-rj-mo";
+
+$('body').css("background",bgColor);
